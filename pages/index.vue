b0e19450326b337980f73ff6921ffdc6553ba06b
Avery Bibeau <avery@wearefine.com>
2022-03-24 09:19:29 -0700

    test

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="index">
+    <p>SSR random(): {{ random }}</p>
+    <p>SSR query: {{ query }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data() {
+    return {
+      random: undefined,
+      query: undefined,
+    }
+  },
+
+  async asyncData({ query }) {
+    let random = Math.random()
+
+    return {
+      random,
+      query: JSON.stringify(query),
+    }
+  },
+})
+</script>
